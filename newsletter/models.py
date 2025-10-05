c759fcf2318674624c5432f85023ef75fa85ab18
mirecloud <info@mirecloud.com>
2025-10-05 15:54:27 -0400

    adding subscriber test

---
+++ newsletter/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Subscriber(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
